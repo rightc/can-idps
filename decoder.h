5cb4b7e047f7bc0991226e525b5f362b42d14fde
right_c <right_c@hotmail.com>
2026-02-12 10:00:29 +0800

    Delete decoder.h

--- decoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAN_IDPS_DECODER_H
-#define CAN_IDPS_DECODER_H
-
-#include "types.h"
-
-/* 将 can_frame 解码为 can_packet */
-void can_decode(const can_frame_t *frame, can_packet_t *out);
-
-#endif /* CAN_IDPS_DECODER_H */
